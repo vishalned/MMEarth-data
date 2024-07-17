2470806d57d5ce3226e702cebda6b8314d3dc180
Vishal Nedungadi <vishalned@gmail.com>
2024-07-17 19:46:54 +0400

    Update data_1M_128.sh

--- bash_scripts/data_1M_128.sh
+++ bash_scripts/data_1M_128.sh
@@ -2,15 +2,15 @@
 
 # URLs of the files to download
 file_urls=(
-    "https://sid.erda.dk/share_redirect/BDqKXwG5RZ/data_1M_128_band_stats.json"
-    "https://sid.erda.dk/share_redirect/BDqKXwG5RZ/data_1M_128_tile_info.json"
-    "https://sid.erda.dk/share_redirect/BDqKXwG5RZ/data_1M_128_splits.json"
-    "https://sid.erda.dk/share_redirect/BDqKXwG5RZ/data_1M_128.h5"
-    "https://sid.erda.dk/share_redirect/BDqKXwG5RZ/LICENSE-data"
+    "https://sid.erda.dk/share_redirect/ChL1BoVEyH/data_1M_v001_band_stats.json"
+    "https://sid.erda.dk/share_redirect/ChL1BoVEyH/data_1M_v001_tile_info.json"
+    "https://sid.erda.dk/share_redirect/ChL1BoVEyH/data_1M_v001_splits.json"
+    "https://sid.erda.dk/share_redirect/ChL1BoVEyH/data_1M_v001.h5"
+    "https://sid.erda.dk/share_redirect/ChL1BoVEyH/LICENSE-data"
 )
 
 # Destination folder to save the downloaded files
-destination_folder="./data_1M_128/"
+destination_folder="./data_1M_v001/"
 
 # Create the destination folder if it doesn't exist
 mkdir -p "$destination_folder"
