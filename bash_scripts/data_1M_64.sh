cc1a207fd100fdfa4c63cd9d73d05ab9c06ae539
Vishal Nedungadi <vishalned@gmail.com>
2024-07-17 19:48:04 +0400

    Update data_1M_64.sh

--- bash_scripts/data_1M_64.sh
+++ bash_scripts/data_1M_64.sh
@@ -2,15 +2,15 @@
 
 # URLs of the files to download
 file_urls=(
-    "https://sid.erda.dk/share_redirect/FZ8k0MINm1/data_1M_64_band_stats.json"
-    "https://sid.erda.dk/share_redirect/FZ8k0MINm1/data_1M_64_tile_info.json"
-    "https://sid.erda.dk/share_redirect/FZ8k0MINm1/data_1M_64_splits.json"
-    "https://sid.erda.dk/share_redirect/FZ8k0MINm1/data_1M_64.h5"
-    "https://sid.erda.dk/share_redirect/FZ8k0MINm1/LICENSE-data"
+    "https://sid.erda.dk/share_redirect/bX5JzPuwJF/data_1M_v001_64_band_stats.json"
+    "https://sid.erda.dk/share_redirect/bX5JzPuwJF/data_1M_v001_64_tile_info.json"
+    "https://sid.erda.dk/share_redirect/bX5JzPuwJF/data_1M_v001_64_splits.json"
+    "https://sid.erda.dk/share_redirect/bX5JzPuwJF/data_1M_v001_64.h5"
+    "https://sid.erda.dk/share_redirect/bX5JzPuwJF/LICENSE-data"
 )
 
 # Destination folder to save the downloaded files
-destination_folder="./data_1M_64/"
+destination_folder="./data_1M_v001_64/"
 
 # Create the destination folder if it doesn't exist
 mkdir -p "$destination_folder"
